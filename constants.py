9aec35355737f26e6e666e1bb39b2c930832aeb8
Chetna_1044 <chetna1044@gmail.com>
2018-11-13 19:24:45 +0530

    constants are assigned

---
+++ constants.py
@@ -0,0 +1,6 @@
+CONSUMER_KEY = 'RncbEudfiWXo4OOJ0bVERoBgD'
+CONSUMER_SECRET = '9CbseICEFSTHMplmiXkwXAmeiCPOomduTqJ0YILSJgUWfeVaF4'
+
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
